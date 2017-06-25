e3e639626b11aeb1606819500d0c56c6d8cc65a2
Joshua Small <jksmall0631@gmail.com>
2017-06-25 10:47:40 -0600

    create routes and basic app layout

--- public/components/App/App.js
+++ public/components/App/App.js
@@ -9,6 +9,13 @@ export default class App extends Component {
   render() {
     return (
       <Nav />
+      <h1>SHARON CLEERE</h1>
+      <h2>CREATIVE IMAGERY THROUGH WORDS AND PHOTOS<h2>
+      <Route exact path='/' component={Welcome}/>
+      <Route exact path='/about' component={About}/>
+      <Route exact path='/travel' component={Travel}/>
+      <Route exact path='/write' component={Write}/>
+      <Route exact path='/photo' component={Photo}/>
     )
   }
 }
