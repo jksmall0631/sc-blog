047741fff2065b7704d34c3a80ba8c80e26679bc
Joshua Small <jksmall0631@gmail.com>
2017-06-25 10:17:24 -0600

    configure index.js to render using router 4

---
+++ public/components/App/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+export default class App extends Component {
+  render(){
+    return(
+      <h1>bla!</h1>
+    )
+  }
+}
