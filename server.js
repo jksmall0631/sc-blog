5cf8d15b701349929944928bc34107aed68f859d
Joshua Small <jksmall0631@gmail.com>
2017-06-25 09:44:50 -0600

    create html structure and configure server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
 })
 
 app.listen(app.get('port'), () => {
