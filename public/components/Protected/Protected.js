10c792c292f4bb77e70f4297e30a833689e7980a
Joshua Small <jksmall0631@gmail.com>
2017-06-28 22:04:21 -0600

    add content to protected page

--- public/components/Protected/Protected.js
+++ public/components/Protected/Protected.js
@@ -4,6 +4,10 @@ export default class Protected extends Component {
   render(){
     return(
       <h1>PROTECTEEEEEED!</h1>
+      <input placeholder='title'></input>
+      <input type='date' placeholder='date'></input>
+      <textarea placeholder='body'></textarea>
+      <button>make that sheeeeiiiitttt!</button>
     )
   }
 }
