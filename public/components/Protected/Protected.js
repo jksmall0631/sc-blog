c15ff6f60587c27a48c62c9a444f54a714702644
Joshua Small <jksmall0631@gmail.com>
2017-06-28 23:27:28 -0600

    create post for blog data

--- public/components/Protected/Protected.js
+++ public/components/Protected/Protected.js
@@ -11,7 +11,14 @@ export default class Protected extends Component {
   }
 
   save(){
-    console.log('bla')
+    const url = 'http://localhost:3000/api/v1/blog'
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({this.state.title, this.state.date, this.state.body})
+    })
+    .then(data => data.json())
+    .then(data => console.log(data))
   }
 
   render(){
@@ -21,7 +28,7 @@ export default class Protected extends Component {
         <input placeholder='title' onChange={(e) => this.setState({title: e.target.value})}></input>
         <input type='date' placeholder='date' onChange={(e) => this.setState({date: e.target.value})}></input>
         <textarea placeholder='body' onChange={(e) => this.setState({body: e.target.value})}></textarea>
-        <button onClick={() => this.save}>make that sheeeeiiiitttt!</button>
+        <button onClick={() => this.save()}>make that sheeeeiiiitttt!</button>
       </div>
     )
   }
