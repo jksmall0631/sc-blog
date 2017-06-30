8c58c2b28114858b0a5b322246bfa5a70eba2b83
Joshua Small <jksmall0631@gmail.com>
2017-06-30 10:22:59 -0600

    add acao and multer

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const aws = require('aws-sdk');
 
-app.set('port', process.env.PORT || 3000)
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header('Access-Control-Allow-Methods', 'GET, POST, PATCH ,DELETE');
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('port', process.env.PORT || 3000)
 
 app.post('/api/v1/auth', (req, res) => {
   const {username, password} = req.body
@@ -39,7 +47,7 @@ let multerS3 = require('multer-s3')
 let upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'adopt-fund',
+    bucket: 'sec-blog',
     region: 'us-west-2',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
