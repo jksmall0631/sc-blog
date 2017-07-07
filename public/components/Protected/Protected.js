4ff4563d6431cad4d8fa2b4face76a41b61577bb
Joshua Small <jksmall0631@gmail.com>
2017-07-06 22:16:15 -0600

    start styling protected component

--- public/components/Protected/Protected.js
+++ public/components/Protected/Protected.js
@@ -8,6 +8,8 @@ import PhotoUpload from '../PhotoUpload/PhotoUpload'
 import Travel from '../Travel/Travel'
 import Photo from '../Photo/Photo'
 
+require('./Protected.css')
+
 export default class Protected extends Component {
   constructor(){
     super()
@@ -29,13 +31,13 @@ export default class Protected extends Component {
     const { photo, title, date, content } = this.state
     return(
       <div>
-        <h1>PROTECTEEEEEED!</h1>
-        
-        <PhotoUpload savePhoto={this.savePhoto} id='file-input' />
-        <input placeholder='title' onChange={(e) => this.setState({title: e.target.value})}></input>
-        <input type='date' placeholder='date' onChange={(e) => this.setState({date: e.target.value})}></input>
-        <textarea placeholder='content' onChange={(e) => this.setState({content: e.target.value})}></textarea>
-        <button onClick={() => this.props.addEntry(photo, title, date, content)}>make that sheeeeiiiitttt!</button>
+        <div className='edit-blog'>
+          <PhotoUpload savePhoto={this.savePhoto} id='file-input' />
+          <input placeholder='title' onChange={(e) => this.setState({title: e.target.value})}></input>
+          <input type='date' placeholder='date' onChange={(e) => this.setState({date: e.target.value})}></input>
+          <textarea placeholder='content' onChange={(e) => this.setState({content: e.target.value})}></textarea>
+          <button onClick={() => this.props.addEntry(photo, title, date, content)}>make that sheeeeiiiitttt!</button>
+        </div>
         <Travel entries={this.props.entries} removeEntry={this.props.removeEntry} />
         <PhotoUpload savePhoto={this.props.savePhoto} id='file-input2' />
         <Photo photos={this.props.photos} removePhoto={this.props.removePhoto} />
