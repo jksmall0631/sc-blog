f6f242b10ac215a7cdc6e00f07563f74a10c6196
Joshua Small <jksmall0631@gmail.com>
2017-08-07 12:53:56 -0600

    update seed data and node version in package.json

--- db/seeds/dev/user.js
+++ db/seeds/dev/user.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   return knex("user").del().then(function() {
     return knex("user").insert([
-      { id: 1, username: "secleere", password: "password" }
+      { id: 1, username: "secleere", password: "19910371" }
     ]);
   });
 };
